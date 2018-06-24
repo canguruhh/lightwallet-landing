aadea1de66af2c5aaa6947825e302608d03f71e6
Sven <sven@mutzl.org>
2018-06-24 09:33:01 +0200

    Chinese language translations

--- js/app.js
+++ js/app.js
@@ -1,22 +1,26 @@
 var app = angular.module('app', ['pascalprecht.translate']);
 
-app.config(['$translateProvider', function ($translateProvider) {
-$translateProvider.useStaticFilesLoader({
-                prefix: 'lang/',
-                suffix: '.json'
-            });
-            $translateProvider.registerAvailableLanguageKeys(['en', 'zh'], {
-                'en-US': 'en',
-                'en-UK': 'en',
-                'zh-ZH': 'zh'
-            });
-            $translateProvider.useSanitizeValueStrategy('escapeParameters')
-		.determinePreferredLanguage()
-		.fallbackLanguage('en')
+app.config(['$translateProvider', function($translateProvider) {
+    $translateProvider.useStaticFilesLoader({
+        prefix: 'lang/',
+        suffix: '.json'
+    });
+    $translateProvider.registerAvailableLanguageKeys(['en', 'zh'], {
+        'en-US': 'en',
+        'en-UK': 'en',
+        'zh-ZH': 'zh'
+    });
+    $translateProvider.useSanitizeValueStrategy('escapeParameters')
+        .determinePreferredLanguage()
+        .fallbackLanguage('en');
 }]);
 
-app.controller('landing', ['$scope','$translate',function($scope, $translate) {
-	$scope.changeLanguage = function (key) {
-		    $translate.use(key);
-		  };
+app.controller('language', ['$scope', '$translate', function($scope, $translate) {
+    $scope.language=$translate.proposedLanguage() || $translate.use();
+    $scope.changeLanguage = function(key) {
+        $translate.use(key).then(function(lang){
+            $scope.language=lang;
+            console.log('change lang to: ' + lang);
+        });
+    };
 }]);
